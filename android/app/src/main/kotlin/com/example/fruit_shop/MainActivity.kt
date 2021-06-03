d5c687a62f9958a21b84f4314570ceed16b04cd5
deepak07082 <sri3193@gmail.com>
2021-06-03 22:19:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/fruit_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fruit_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fruit shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: IntroPage(),
+    );
+  }
+}
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
